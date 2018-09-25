217e10a16c7a245896cde16f994c592072b1d785
Eugene Oparyshev <eop@sitecore.net>
2018-09-25 17:17:47 +0300

    Derive from the default code functionality

--- src/Sitecore.Support.224113/EmailCampaign/cd/sitecore_modules/Web/EXM/Unsubscribe.cs
+++ src/Sitecore.Support.224113/EmailCampaign/cd/sitecore_modules/Web/EXM/Unsubscribe.cs
@@ -1,24 +1,14 @@
-﻿namespace Sitecore.Support.EmailCampaign.cd.sitecore_modules.Web.EXM
+﻿namespace Sitecore.Support.EmailCampaign.Cd.sitecore_modules.Web.EXM
 {
+    using Sitecore.EmailCampaign.Cd;
     using Sitecore.EmailCampaign.Model.Messaging;
     using Sitecore.Modules.EmailCampaign.Core;
     using Sitecore.XConnect;
     using System;
-
-    public class Unsubscribe
+    
+    public class Unsubscribe : Sitecore.EmailCampaign.Cd.sitecore_modules.Web.EXM.Unsubscribe
     {
-        public class Unsubscribe : UnsubscribeMessageEventPage
-        {
-            protected override string UnsubscribeContact(ContactIdentifier contactIdentifier, Guid messageID)
-            {
-                this.ClientApiService.Unsubscribe(new UnsubscribeMessage
-                {
-                    AddToGlobalOptOutList = false,
-                    ContactIdentifier = contactIdentifier,
-                    MessageId = messageID
-                });
-                return ExmContext.Message.ManagerRoot.GetConfirmativePageUrl() ?? "/";
-            }
-        }
+       
     }
+
 }
--- src/Sitecore.Support.224113/EmailCampaign/cd/sitecore_modules/Web/EXM/UnsubscribeFromAll.cs
+++ src/Sitecore.Support.224113/EmailCampaign/cd/sitecore_modules/Web/EXM/UnsubscribeFromAll.cs
@@ -1,37 +1,13 @@
-﻿namespace Sitecore.Support.EmailCampaign.cd.sitecore_modules.Web.EXM
+﻿namespace Sitecore.Support.EmailCampaign.Cd.sitecore_modules.Web.EXM
 {
+    using Sitecore.EmailCampaign.Cd;
     using Sitecore.EmailCampaign.Model.Messaging;
     using Sitecore.Modules.EmailCampaign.Core;
     using Sitecore.XConnect;
     using System;
 
-    public class UnsubscribeFromAll : UnsubscribeMessageEventPage
+    public class UnsubscribeFromAll : Sitecore.EmailCampaign.Cd.sitecore_modules.Web.EXM.UnsubscribeFromAll
     {
-        protected internal override string NoMessageIdLogMesssage
-        {
-            get
-            {
-                return "Cannot handle global unsubscription event. No message id was provided in the request.";
-            }
-        }
-
-        protected internal override string NoContactIdLogMessage
-        {
-            get
-            {
-                return "Cannot handle global unsubscription event. No contact id was provided in the request and no default subscription page is configured.";
-            }
-        }
-
-        protected override string UnsubscribeContact(ContactIdentifier contactIdentifier, Guid messageID)
-        {
-            this.ClientApiService.Unsubscribe(new UnsubscribeMessage
-            {
-                AddToGlobalOptOutList = true,
-                ContactIdentifier = contactIdentifier,
-                MessageId = messageID
-            });
-            return ExmContext.Message.ManagerRoot.GetConfirmativePageUrl() ?? "/";
-        }
+       
     }
 }
