e78e3e032c62d1dd4cba7d08b6e9497770ab563b
Eugene Oparyshev <eop@sitecore.net>
2018-09-25 17:05:34 +0300

    default implementation

---
+++ src/Sitecore.Support.224113/EmailCampaign/cd/sitecore_modules/Web/EXM/Unsubscribe.cs
@@ -0,0 +1,24 @@
+﻿namespace Sitecore.Support.EmailCampaign.cd.sitecore_modules.Web.EXM
+{
+    using Sitecore.EmailCampaign.Model.Messaging;
+    using Sitecore.Modules.EmailCampaign.Core;
+    using Sitecore.XConnect;
+    using System;
+
+    public class Unsubscribe
+    {
+        public class Unsubscribe : UnsubscribeMessageEventPage
+        {
+            protected override string UnsubscribeContact(ContactIdentifier contactIdentifier, Guid messageID)
+            {
+                this.ClientApiService.Unsubscribe(new UnsubscribeMessage
+                {
+                    AddToGlobalOptOutList = false,
+                    ContactIdentifier = contactIdentifier,
+                    MessageId = messageID
+                });
+                return ExmContext.Message.ManagerRoot.GetConfirmativePageUrl() ?? "/";
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.224113/EmailCampaign/cd/sitecore_modules/Web/EXM/UnsubscribeFromAll.cs
@@ -0,0 +1,37 @@
+﻿namespace Sitecore.Support.EmailCampaign.cd.sitecore_modules.Web.EXM
+{
+    using Sitecore.EmailCampaign.Model.Messaging;
+    using Sitecore.Modules.EmailCampaign.Core;
+    using Sitecore.XConnect;
+    using System;
+
+    public class UnsubscribeFromAll : UnsubscribeMessageEventPage
+    {
+        protected internal override string NoMessageIdLogMesssage
+        {
+            get
+            {
+                return "Cannot handle global unsubscription event. No message id was provided in the request.";
+            }
+        }
+
+        protected internal override string NoContactIdLogMessage
+        {
+            get
+            {
+                return "Cannot handle global unsubscription event. No contact id was provided in the request and no default subscription page is configured.";
+            }
+        }
+
+        protected override string UnsubscribeContact(ContactIdentifier contactIdentifier, Guid messageID)
+        {
+            this.ClientApiService.Unsubscribe(new UnsubscribeMessage
+            {
+                AddToGlobalOptOutList = true,
+                ContactIdentifier = contactIdentifier,
+                MessageId = messageID
+            });
+            return ExmContext.Message.ManagerRoot.GetConfirmativePageUrl() ?? "/";
+        }
+    }
+}
