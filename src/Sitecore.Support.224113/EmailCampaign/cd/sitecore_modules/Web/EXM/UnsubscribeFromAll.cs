c7410ab9f4f3c7b96c4c4b48c462941ae440b085
Eugene Oparyshev <eop@sitecore.net>
2018-09-25 17:23:26 +0300

    Github patch

--- src/Sitecore.Support.224113/EmailCampaign/cd/sitecore_modules/Web/EXM/UnsubscribeFromAll.cs
+++ src/Sitecore.Support.224113/EmailCampaign/cd/sitecore_modules/Web/EXM/UnsubscribeFromAll.cs
@@ -1,7 +1,12 @@
 ﻿namespace Sitecore.Support.EmailCampaign.Cd.sitecore_modules.Web.EXM
 {
+    using Sitecore.Data;
+    using Sitecore.Data.Items;
     using Sitecore.EmailCampaign.Cd;
+    using Sitecore.Links;
     using Sitecore.EmailCampaign.Model.Messaging;
+    using Sitecore.Sites;
+    using Sitecore.Web;
     using Sitecore.Modules.EmailCampaign.Core;
     using Sitecore.XConnect;
     using System;
@@ -11,6 +16,26 @@
         protected override string VerifyContactSubscriptions(ContactIdentifier contactIdentifier, Guid messageID)
         {
             return base.VerifyContactSubscriptions(contactIdentifier, messageID);
+            #region Sitecore.Support.224113
+            string alreadyItemPath = ExmContext.Message.ManagerRoot.Settings.AlreadyUnsubscribedPage;
+            Item alreadyItem = Database.GetDatabase("master").GetItem(alreadyItemPath);
+            string itemUrl = String.Empty;
+            SiteInfo site = null;
+            var siteInfoList = Sitecore.Configuration.Factory.GetSiteInfoList();
+            foreach (SiteInfo siteInf in siteInfoList)
+            {
+                if (siteInf.RootPath.Trim() != "" && siteInf.StartItem.Trim() != "" && alreadyItem.Paths.FullPath.Trim().Contains(siteInf.RootPath.Trim() + siteInf.StartItem.Trim()))
+                {
+                    site = siteInf;
+                }
+            }
+
+            using (new SiteContextSwitcher(SiteContextFactory.GetSiteContext(site.Name)))
+            {
+                itemUrl = LinkManager.GetItemUrl(alreadyItem);
+            }
+            return itemUrl;
+            #endregion
         }
     }
 }
